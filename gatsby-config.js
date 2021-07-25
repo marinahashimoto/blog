be83e5e37890817cb3b949187ba41dc06b60cd70
marinahashimoto <m.hashimoto0217@gmail.com>
2021-07-25 23:14:19 +0900

    fix deploy command

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,8 +112,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Hashimoto's Blog`,
+        short_name: `Hashimoto's Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
